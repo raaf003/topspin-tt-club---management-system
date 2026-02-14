a65a095db42cd07da95de3e77fa5dd36a5aaf0e6
Rafaqat Rasool <raaf003@gmail.com>
2026-02-14 12:35:52 +0530

    test commit to trigger build on cloud run

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -16,6 +16,7 @@ const app = (0, express_1.default)();
 const PORT = process.env.PORT || 5000;
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
+
 // Routes
 app.use('/api/auth', authRoutes_1.default);
 app.use('/api/players', playerRoutes_1.default);
